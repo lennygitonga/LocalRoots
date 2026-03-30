7aef524f8490148e0117e4a25056852baf4aaaad
Lenny Gitonga <lennygitonga07@gmail.com>
2026-03-30 11:06:36 +0300

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ReportPage from "./pages/ReportPage";
+import HelpPage from "./pages/HelpPage";
+import ChatPage from "./pages/ChatPage";
+
 function App() {
   return (
-    <div>
-      <h1>Local Roots</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/help" element={<HelpPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </>
   );
 }
 
